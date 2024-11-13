febe6e5d13b33400241a09d5fc6b770cec111c59
sergiosaraivajr <sergiosaraivajr01@gmail.com>
2024-11-13 20:48:51 -0300

    Criação do component TittleHome

---
+++ src/app/components/CarouselHome.tsx
@@ -0,0 +1,19 @@
+function CarouselHome() {
+  return (
+    <div className="flex-col justify-center items-center gap-6 flex">
+      <h1 className="text-blackHeadLine font-bold text-5xl text-center font-['DM Sans'] leading-[72px]">
+        Seja Bem-vido à SampaioForce
+      </h1>
+      <h5 className="text-center text-black text-lg font-normal font-['DM Sans'] leading-[27px]">
+        Somos uma Startup dedicada a oferecer soluções inovadoras. Nossa missão
+        é proporcionar excelência e resultados excepcionais para nossos
+        clientes.
+      </h5>
+      <button className="w-40 h-12 rounded-lg bg-brandGreenLight hover:bg-brandGreenHover">
+        Saiba Mais
+      </button>
+    </div>
+  );
+}
+
+export default CarouselHome;

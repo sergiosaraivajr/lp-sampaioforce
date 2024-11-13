da63afb5f1249b0427c3aff78efd7e45591a30f3
sergiosaraivajr <sergiosaraivajr01@gmail.com>
2024-11-13 20:56:15 -0300

    FIX: Correção do component NavBar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 function NavBar() {
   return (
-    <header className="h-[88px] bg-black shadow-inner flex-col justify-center flex">
+    <header className="w-full h-[88px] bg-black shadow-inner flex-col justify-center flex">
       <div className="self-stretch justify-between items-center inline-flex px-16">
         <div className="flex items-center flex-shrink-0 text-white mr-6 hover:cursor-pointer">
           <img
@@ -11,13 +11,13 @@ function NavBar() {
         </div>
         <nav className="justify-center flex gap-16">
           <ul className="items-center flex text-gray6 font-normal md:flex-row gap-16 hover:cursor-pointer list-item:h-12">
-            <li>Início</li>
-            <li>Sobre nós</li>
-            <li>Serviços</li>
-            <li>Projetos</li>
-            <li>Blog</li>
+            <li className="hover:text-brandGreenHover">Início</li>
+            <li className="hover:text-brandGreenHover">Sobre nós</li>
+            <li className="hover:text-brandGreenHover">Serviços</li>
+            <li className="hover:text-brandGreenHover">Projetos</li>
+            <li className="hover:text-brandGreenHover">Blog</li>
           </ul>
-          <button className="w-40 h-12 rounded-lg bg-gray6">
+          <button className="w-40 h-12 rounded-lg bg-gray6 hover:bg-brandGreenHover">
             Fale conosco
           </button>
         </nav>

48be661074af01158a6b16b2e954133f0e6fd784
sergiosaraivajr <sergiosaraivajr01@gmail.com>
2024-11-13 16:32:50 -0300

    padrão de cores no tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brandGreenLight: "#AAD813",
+        brandGreenHover: "#97c00d",
+        black: "#000000",
+        blackHeadLine: "#111111",
+        White: "#FFFFFF",
+        whiteParagraph: "#F2F2F2",
+        gray1: "#333333",
+        gray2: "#4F4F4F",
+        Gray3: "#828282",
+        gray5: "#E0E0E0",
+        gray6: "#F2F2F2",
       },
     },
   },

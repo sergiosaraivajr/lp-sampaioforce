11047150e44ada5e1bfc18b5c3666638bfcf1a05
sergiosaraivajr <sergiosaraivajr01@gmail.com>
2024-11-14 00:59:51 -0300

    Adição da Pagee Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
+import Projects from "./pages/Projects";
 
 export default function Home() {
   return (
     <main>
       <HomePage />
       <AboutUs />
+      <Projects />
     </main>
   );
 }

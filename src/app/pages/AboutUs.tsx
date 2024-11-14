a4e863845f64b0b6a43eff218fed9dd6aaba22df
sergiosaraivajr <sergiosaraivajr01@gmail.com>
2024-11-14 00:25:15 -0300

    Criação da Page AboutUs

---
+++ src/app/pages/AboutUs.tsx
@@ -0,0 +1,76 @@
+function AboutUs() {
+  return (
+    <div className="w-full h-[810px] px-16 py-40 bg-gray6 flex-col justify-center items-center gap-[88px] inline-flex">
+      <div className="h-[170px] justify-center items-start gap-8 inline-flex">
+        <div className="grow shrink basis-0 flex-col justify-start items-start gap-6 inline-flex">
+          <div className="self-stretch h-[170px] flex-col justify-start items-start gap-4 flex">
+            <div className="w-12 h-12 relative">
+              <img src="/cubo.svg"></img>
+            </div>
+            <div className="self-stretch h-[106px] flex-col justify-start items-start gap-6 flex">
+              <div className="self-stretch text-black text-2xl font-dmsansbold leading-[33.60px]">
+                Inovação
+              </div>
+              <div className="self-stretch text-black text-base font-dmsansregular leading-normal">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grow shrink basis-0 flex-col justify-start items-start gap-6 inline-flex">
+          <div className="self-stretch h-[170px] flex-col justify-start items-start gap-4 flex">
+            <div className="w-12 h-12 relative">
+              <img src="/cubo.svg"></img>
+            </div>
+            <div className="self-stretch h-[106px] flex-col justify-start items-start gap-6 flex">
+              <div className="self-stretch text-black text-2xl font-dmsansbold leading-[33.60px]">
+                Compromisso
+              </div>
+              <div className="self-stretch text-black text-base font-dmsansregular leading-normal">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grow shrink basis-0 flex-col justify-start items-start gap-6 inline-flex">
+          <div className="self-stretch h-[170px] flex-col justify-start items-start gap-4 flex">
+            <div className="w-12 h-12 relative">
+              <img src="/cubo.svg"></img>
+            </div>
+            <div className="self-stretch h-[106px] flex-col justify-start items-start gap-6 flex">
+              <div className="self-stretch text-black text-2xl font-dmsansbold leading-[33.60px]">
+                Transparência
+              </div>
+              <div className="self-stretch text-black text-base font-dmsansregular leading-normal">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-[0px] border border-gray5"></div>
+      <div className="flex-row h-36 justify-start items-start gap-20 inline-flex">
+        <div className="grow shrink basis-0 flex-col justify-start items-start gap-4 inline-flex">
+          <div className="self-stretch text-black text-[40px] font-dmsansbold leading-[48px]">
+            Descubra Como Nossos Recursos Podem Transformar Sua Experiência e
+            Resultados
+          </div>
+        </div>
+        <div className="grow shrink basis-0 flex-col justify-start items-start gap-6 inline-flex">
+          <div className="self-stretch text-black text-lg font-dmsansregular leading-[27px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique. Duis cursus, mi quis
+            viverra ornare, eros dolor interdum nulla, ut commodo diam libero
+            vitae erat. Suspendisse varius enim in eros elementum tristique.
+            Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
